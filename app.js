b4afb53ff03fe97466a4921cba40b3b9b2d2eb14
Alex Kemboi <akemboi@KWFTBANK.INT>
2023-04-04 04:41:36 +0300

    created a download button

--- app.js
+++ app.js
@@ -46,31 +46,40 @@ const footer = document.getElementById("footer-section");
 const footerHTML = `
   <div class="row">
     <div class="col-4 ">
-      <h4>Quick Links</h4>
-      <ul>
-        <h5 ><a href="#about" class="text-dark"><i class="fas fa-user text-dark"></i> About</a></h5>
-        <h5 class="text-dark"><a href="#experience" class="text-dark"><i class="fas fa-briefcase text-dark"></i> Experience</a></h5>
-        <h5 class="text-dark"><a href="#education" class="text-dark"><i class="fas fa-graduation-cap text-dark"></i> Education</a></h5>
-        <h5 class="text-dark"><a href="#skills" class="text-dark"><i class="fas fa-code text-dark"></i> Skills</a></h5>
-        <h5 class="text-dark"><a href="#contact" class="text-dark"><i class="fas fa-envelope text-dark"></i> Contact</a></h5>
-    </ul>
-
+        <div class="card" >
+            <div class="card-body" >    
+                <h4>Quick Links</h4>
+                    <ul>
+                        <h5 ><a href="#about" class="text-dark"><i class="fas fa-user text-dark"></i> About</a></h5>
+                        <h5 class="text-dark"><a href="#experience" class="text-dark"><i class="fas fa-briefcase text-dark"></i> Experience</a></h5>
+                        <h5 class="text-dark"><a href="#education" class="text-dark"><i class="fas fa-graduation-cap text-dark"></i> Education</a></h5>
+                        <h5 class="text-dark"><a href="#skills" class="text-dark"><i class="fas fa-code text-dark"></i> Skills</a></h5>
+                        <h5 class="text-dark"><a href="#contact" class="text-dark"><i class="fas fa-envelope text-dark"></i> Contact</a></h5>
+                    </ul>
+            </div>
+        </div>
     </div>
     <div class="col-4 ">
-      <h4>Follow Me</h4>
-      <ul class="social-media">
-        <h5><a href="#"class="text-dark"><i class="fas fa-envelope text-dark"></i>Email</a></h5>
-        <h5><a href="#"class="text-dark"><i class="fab fa-twitter text-dark"></i>Twitter</a></h5>
-        <h5><a href="#"class="text-dark"><i class="fab fa-linkedin-in text-dark"></i>Linkedin</a></h5>
-        <h5><a href="#"class="text-dark"><i class="fab fa-facebook text-dark"></i>Facebook</a></h5>
-      </ul>
+            <div class="card" >
+                    <div class="card-body">
+                    <h4>Follow Me</h4>
+                    <ul class="social-media">
+                      <h5><a href="#"class="text-dark"><i class="fas fa-envelope text-dark"></i>Email</a></h5>
+                      <h5><a href="#"class="text-dark"><i class="fab fa-twitter text-dark"></i>Twitter</a></h5>
+                      <h5><a href="#"class="text-dark"><i class="fab fa-linkedin-in text-dark"></i>Linkedin</a></h5>
+                      <h5><a href="#"class="text-dark"><i class="fab fa-facebook text-dark"></i>Facebook</a></h5>
+                    </ul>
+                    </div>
+            </div>
     </div>
 
     <div class="col-4 ">
-    <img src="/caro2.jpg"  class=" img-fluid rounded-circle" alt="..." style="width: 5.08cm; height: 6.35cm;"">
-                          
-                         
-  </div>
+        <div class="card" >
+            <div class="card-body">
+                <img src="/caro2.jpg"  class=" img-fluid rounded-circle" alt="..." style="width: 5.08cm; height: 6.35cm;"">    
+            </div>
+        </div> 
+    </div>
   </div>
   <p class="text-center">&copy; 2023 Carolyne Kimetto. All rights reserved.</p>
 `;
