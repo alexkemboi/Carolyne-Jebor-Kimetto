888351a10885761119cfdb538ea6974890b45bcd
Alex Kemboi <akemboi@KWFTBANK.INT>
2023-04-15 15:03:15 +0300

    updated the appearance

--- app.js
+++ app.js
@@ -37,45 +37,44 @@ const footer = document.getElementById("footer-section");
 
 // Create the HTML structure for the footer
 const footerHTML = `
-<div class="container">
 <div class="row">
   <div class="col-sm-4">
-    <div class="card border-0">
+    <div class="card border-0 bg-dark">
       <div class="card-body">
-        <h4 class="text-dark">Quick Links</h4>
+        <h4 class="text-white">Quick Links</h4>
         <ul class="list-unstyled">
-          <li><a href="#about" class="text-dark"><i class="fas fa-user text-dark"></i> About</a></li>
-          <li><a href="#experience" class="text-dark"><i class="fas fa-briefcase text-dark"></i> Experience</a></li>
-          <li><a href="#education" class="text-dark"><i class="fas fa-graduation-cap text-dark"></i> Education</a></li>
-          <li><a href="#skills" class="text-dark"><i class="fas fa-code text-dark"></i> Skills</a></li>
-          <li><a href="#contact" class="text-dark"><i class="fas fa-envelope text-dark"></i> Contact</a></li>
+          <li><a href="#about" class="text-white"><i class="fas fa-user text-white"></i> About</a></li>
+          <li><a href="#experience" class="text-white"><i class="fas fa-briefcase text-white"></i> Experience</a></li>
+          <li><a href="#education" class="text-white"><i class="fas fa-graduation-cap text-white"></i> Education</a></li>
+          <li><a href="#skills" class="text-white"><i class="fas fa-code text-white"></i> Skills</a></li>
+          <li><a href="#contact" class="text-white"><i class="fas fa-envelope text-white"></i> Contact</a></li>
         </ul>
       </div>
     </div>
   </div>
   <div class="col-sm-4">
-    <div class="card border-0">
+    <div class="card border-0 bg-dark">
       <div class="card-body">
-        <h4 class="text-dark">Follow Me</h4>
+        <h4 class="text-white">Follow Me</h4>
         <ul class="list-unstyled">
-          <li><a href="#" class="text-dark"><i class="fas fa-envelope text-dark"></i> Email</a></li>
-          <li><a href="#" class="text-dark"><i class="fab fa-twitter text-dark"></i> Twitter</a></li>
-          <li><a href="#" class="text-dark"><i class="fab fa-linkedin-in text-dark"></i> Linkedin</a></li>
-          <li><a href="#" class="text-dark"><i class="fab fa-facebook text-dark"></i> Facebook</a></li>
+          <li><a href="#" class="text-white"><i class="fas fa-envelope text-white"></i> Email</a></li>
+          <li><a href="#" class="text-white"><i class="fab fa-twitter text-white"></i> Twitter</a></li>
+          <li><a href="#" class="text-white"><i class="fab fa-linkedin-in text-white"></i> Linkedin</a></li>
+          <li><a href="#" class="text-white"><i class="fab fa-facebook text-white"></i> Facebook</a></li>
         </ul>
       </div>
     </div>
   </div>
   <div class="col-sm-4">
-    <div class="card border-0">
+    <div class="card border-0 bg-dark">
       <div class="card-body">
-        <img src="/caro2.jpg" class="img-fluid rounded-circle" alt="..." style="width: 100%; max-width: 150px;">
+      <a href="/caro_cv.pdf" download class="btn bg-secondary btn-dark mb-3">Download CV</a>
       </div>
     </div>
   </div>
 </div>
-<p class="text-center text-dark">&copy; 2023 Carolyne Kimetto. All rights reserved.</p>
-</div>
+<p class="text-center text-white">&copy; 2023 Carolyne Kimetto. All rights reserved.</p>
+
 `;
 
 // Add the HTML code to the footer element
